6bc4833de265dcbea585b72bb0521925d892ad35
Phuong Tran <ptran816@uw.edu>
2020-10-30 00:43:42 -0700

    phone number verification

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const [phoneNumber, setPhoneNumber] = useState('')
-  const [accessCode, setAccessCode] = useState('')
+  const [showVerification, setShowVerification] = useState(false)
 
   const sendPhoneNumber = () => {
-    const url = 'localhost:8080/generate-code'
+    // only fetch when user entered something
+    if (!phoneNumber) return
+
+    const url = 'http://localhost:8080/generate-code'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({phoneNumber})
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      if(json.accessCode) {
+        setShowVerification(true)
+      }else {
+        throw {msg: "Unable to send a message. Make you you have a correct phone number"}
+      }
+    }).catch(err => {
+      alert(err.msg)
+    })
+  }
+  return (
+    <div style={{textAlign: 'center'}}>
+      <div className="justify-content-center">
+        <label>Phone number:</label>
+        <input onChange={e => setPhoneNumber(e.target.value)} />
+      </div>
+      <button type="button" className="btn btn-primary " onClick={sendPhoneNumber}>Send</button>
+      {showVerification ? <Verification phoneNumber={phoneNumber} setShowVerification={setShowVerification} /> : null}
+    </div>
+  );
+}
+export default App;
+
+function Verification({ phoneNumber, setShowVerification }) {
+
+  const [accessCode, setAccessCode] = useState('')
+
+  const verifyCode = () => {
+    const url = 'http://localhost:8080/validate'
     fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(
-        {phoneNumber}
+        {accessCode, phoneNumber}
       )
-    }).then(res => {
-      return res.json()
-    }).then(json => {
-      console.log(json)
+    }).then(response => {
+        return response.json()
+      }).then(json => {
+        if(json.success) {
+          alert("Phone number vefified!")
+          setShowVerification(false)
+        }else {
+          alert("Incorrect Code. Enter the code again or resend.")
+        }
+    }).catch(err => {
+      console.log(err)
     })
   }
-  return (
-    <div className="App">
-      <div className="m-auto">
-        <label>Phone number:</label>
-        <input onChange={e => setPhoneNumber(e.target.value)}/>
-      </div>
-      <div>
-        <label> Access code:</label>
-        <input/>
-      </div>
-      <button type="button" class="btn btn-outline-secondary" onClick={sendPhoneNumber}>Send</button>
-    </div>
-  );
-}
 
-export default App;
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className='d-flex flex-column' style={{textAlign: 'center'}}>
+          <label> Enter the 6-digit code we sent to</label>
+          <label style={{ color: 'blue' }}>{phoneNumber}</label>
+          <input type="text" className="form-control mb-2 mt-2" placeholder=" 6-digit code" value={accessCode} onChange={e => setAccessCode(e.target.value)}></input>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button type="button" className="btn btn-secondary mr-2" onClick={() => setShowVerification(false)}> Cancel</button>
+          <button type="button" className="btn btn-primary" onClick={verifyCode}> Verify</button>
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- server/app.js
+++ server/app.js
@@ -1,38 +1,79 @@
+require('dotenv').config();
 const express = require('express')
+const firebase = require('firebase');
 const randomstring = require("randomstring")
 const app = express()
 const port = 8080
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')('ACd5757c3e4c3fb3e8e5b54f456d164f9e', '43b6e95a81b82353503539c057f7431c');
+// https://www.twilio.com/blog/2017/01/how-to-set-environment-variables.html
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken);
 
-var twilio = require('twilio');
-var client = new twilio('ACd5757c3e4c3fb3e8e5b54f456d164f9e', '43b6e95a81b82353503539c057f7431c');
+//config Firebase
+var config = {
+  apiKey: "AIzaSyB1t9FRDoBXk8FL1hL837UAoAz_VCBwh0Y",
+  authDomain: "skipli-d6fe8.firebaseapp.com",
+  databaseURL: "https://skipli-d6fe8.firebaseio.com/",
+  storageBucket: "gs://skipli-d6fe8.appspot.com"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+// read and write to Firebase
+async function writeData(phoneNumber, accessCode) {
+  return database.ref('phones/' + phoneNumber).set({
+    accessCode
+  }).then( () => {
+    return true
+  }).catch(() => {
+    return false
+  })
+}
+async function readData(phoneNumber) {
+  return database.ref('phones/' + phoneNumber).once('value').then(function (snapshot) {
+    return snapshot.val()
+  })
+}
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.post('/generate-code', (req, res) => {
+app.post('/generate-code', async (req, res) => {
   // assuming the phone number recieved is valid
   const phoneNumber = req.body.phoneNumber
-  const accessCode = randomstring.generate({length: 6, charset: 'numeric'})
+  const accessCode = randomstring.generate({ length: 6, charset: 'numeric' })
+  const ok = await writeData(phoneNumber, accessCode)
+  // failed to write to Firebase
+  if (!ok) {
+    res.json({ success: false })
+  }
 
   client.messages.create({
-     body: `Your verification code is: ${accessCode}`,
-     from: '+14153845479',
-     to: phoneNumber
-  }).then(message => {
-    console.log(message.sid)
-    res.json({accessCode})
+    body: `Your verification code is: ${accessCode}`,
+    from: '+14153845479',
+    to: phoneNumber
+  }).then(() => {
+    res.json({ accessCode})
+  }).catch(() => {
+    res.json({ accessCode: null })
   });
+})
 
-
-  // if (phoneNumber) {
-  // }
+app.post('/validate', async (req, res) => {
+  const { phoneNumber, accessCode } = req.body
+  const dbResult = await readData(phoneNumber)
+  if (dbResult && dbResult.accessCode === accessCode) {
+    await writeData(phoneNumber, "")
+    res.json({ success: true })
+  } else {
+    res.json({ success: false })
+  }
 })
 
 app.listen(port, () => {
